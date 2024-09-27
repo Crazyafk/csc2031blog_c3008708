3b76b9b313b1e4d746002a5a410576a942fd473b
Crazyafk <fel3web@gmail.com>
2024-09-27 15:38:13 +0100

    move index.html into home folder

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home/index.html')
 
 if __name__ == '__main__':
     app.run()
