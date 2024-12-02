868f2f8e0d1bf6c10130c069dba1289d5dff863e
Crazyafk <fel3web@gmail.com>
2024-12-02 16:42:01 +0000

    refactor blank lines in app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from config import app
 from flask import render_template
 
+
 @app.route('/')
 def index():
     return render_template('home/index.html')
 
+
 @app.errorhandler(429)
 def http429(e):
     return render_template('errors/429.html')
+
 
 if __name__ == '__main__':
     app.run()
